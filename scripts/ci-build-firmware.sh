b629ea45c1dad31f52fccdf580ebc904af947cd7
S13391355 <aeltherington@Desktop.localdomain>
2022-04-13 12:50:07 +1200

    personalised settings

--- scripts/ci-build-firmware.sh
+++ scripts/ci-build-firmware.sh
@@ -6,7 +6,7 @@ set -e
 
 : "${TAG:="$(date "+%Y%m%d%H%M%S")"}"
 
-subdir="keyboards/keychron/k6/keymaps/ansi-josh"
+subdir="keyboards/keychron/k6/keymaps/ansi-sleepless"
 
 if [[ -d "qmk_firmware/$subdir" ]]; then
   rm -rf "qmk_firmware/$subdir"
@@ -15,5 +15,5 @@ fi
 cp -rp "$subdir" qmk_firmware/keyboards/keychron/k6/keymaps
 
 (cd qmk_firmware &&
-  make -j4 keychron/k6/rgb/ansi:ansi-josh COLOR=false &&
-  mv keychron_k6_rgb_ansi_ansi-josh.bin ../build/"keychron-k6-ansi-josh-$TAG.bin")
+  make -j4 keychron/k6/rgb/ansi:ansi-sleepless COLOR=false &&
+  mv keychron_k6_rgb_ansi_ansi-sleepless.bin ../build/"keychron-k6-ansi-sleepless-$TAG.bin")
